f7029a91c5f2a9d874c0a2c208373b695211e92e
fatemeh311 <fatemeh.fathi1992@yahoo.com>
2023-02-09 14:24:44 +0330

    change auth guard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+  user: any = null;
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.route.data
+      .subscribe((data: any) => {
+        this.user = data;
+      });
+  }
 
 }
--- src/app/services/guards/auth.guard.ts
+++ src/app/services/guards/auth.guard.ts
@@ -1,20 +1,44 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) { }
+  url = 'http://localhost:4200/api/';
+  constructor(private router: Router,private http: HttpClient) { }
 
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (localStorage.getItem('access_token')) {
-      return true;
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | any {
+    const id = sessionStorage.getItem('id');
+    const jwtToken = sessionStorage.getItem('jwt');
+    const reqHeader = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + jwtToken,
+      })
+    };
+
+    if (jwtToken) {
+      return this.http.get(`${this.url}user/${id}`, reqHeader).pipe(
+        map(res => {
+          if (res === Number(id)) {
+            return true;
+          } else {
+            this.router.navigateByUrl('login');
+            return false;
+          }
+        }),
+        catchError((err) => {
+          return of(false);
+        })
+      );
+    } else {
+      this.router.navigateByUrl('dashboard');
+      return false;
     }
-
-    this.router.navigate(['dashboard']);
-    return false;
   }
+ 
   
 }
