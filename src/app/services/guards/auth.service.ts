a4e153546df59e2154f7676531d47d8a600490a0
fatemeh311 <fatemeh.fathi1992@yahoo.com>
2023-02-09 17:43:17 +0330

    add jwt-decode

--- src/app/services/guards/auth.service.ts
+++ src/app/services/guards/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as jwt_decode from "jwt-decode";
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +24,9 @@ export class AuthService {
   logout() {
     localStorage.removeItem('access_token');
   }
+  // getDecodedAccessToken(): any {
+  //   return jwt_decode(this.getRawAuthToken(AuthTokenType.AccessToken));
+  // }
 
   public get loggedIn(): boolean {
     return (localStorage.getItem('access_token') !== null);
